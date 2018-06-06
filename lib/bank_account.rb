9b39c49b2027634c538cafe29b9aa1efe6ed853c
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 03:29:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,4 +8,20 @@ class BankAccount
     @status = "open";
     @balance = 1000
   end
+
+  def deposit(amount)
+    @balace += amount;
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open"
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
