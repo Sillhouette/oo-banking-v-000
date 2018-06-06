173366d208b1bead32f7e3031c856da1340e3d69
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 04:32:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,4 +23,9 @@ class Transfer
       "Transaction rejected. Please check your account balance.";
     end
   end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(@amount) && @receiver -= @amount
+  end
 end
