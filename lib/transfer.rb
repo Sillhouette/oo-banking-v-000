055db4f561e1a810ebe8559a36151b43468da723
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 04:32:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @sender.deposit(@amount) && @receiver -= @amount
+      @sender.deposit(@amount) && @receiver.balance -= @amount
     end
   end
 end
