b360ca3d02e7ccfc75e6d66182c58d43b3688f42
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 04:29:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,12 @@ class Transfer
   end
 
   def execute_transaction
-    ((@sender.balance >= @amount) && @status == "pending") ? @receiver.deposit(@amount) && (@sender.balance -= @amount) && @status = "complete" :
-    @status = "rejected" && "Transaction rejected. Please check your account balance."
+    if @sender.balance >= amount && @status == "pending";
+      @receiver.deposit(@amount);
+      @sender.balance -= @amount;
+      @status = "complete";
+    else
+    @status = "rejected";
+    "Transaction rejected. Please check your account balance.";
   end
 end
